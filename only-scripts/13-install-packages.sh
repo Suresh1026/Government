15780c40b842dcbdd97a8aa969a269137c76a93e
Suresh Dayyala <suresh.xlmac@gmail.com>
2024-01-29 13:02:19 +0530

    13th variable added

---
+++ only-scripts/13-install-packages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+if [ ID -ne 0 ]
+then 
+    echo -e "$R error: plese run this script with root user $N"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+echo "All arguments passed: $@"

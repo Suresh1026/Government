882be5a951c520a28b3e187f6ba15d4939be85a6
Suresh Dayyala <suresh.xlmac@gmail.com>
2024-01-29 13:39:26 +0530

    13th variable added

--- only-scripts/13-install-packages.sh
+++ only-scripts/13-install-packages.sh
@@ -3,7 +3,22 @@
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R Failed $N"
+    else
+        echo -e "$2 ...$G Success $N"
+    fi
+}
 
 if [ $ID -ne 0 ]
 then 
@@ -14,3 +29,15 @@ else
 fi
 
 echo "All arguments passed: $@"
+
+for package in $@
+do
+    yum list installed $package &>> $LOGFILE # check package installed or not
+    if [ $? -ne 0 ] # if not installed
+then
+    yum install $package -y &>> $LOGFILE # install the package
+    VALIDATE $? "Installation of $package" # validate
+else
+    echo -e "$package is already instaalled ... $Y SKIPPING $N"
+fi
+done
